8ab0b142e7993a9598a01079aff2ca7c2b900974
tgtesh <tgteshe53@gmail.com>
2022-07-05 23:23:32 -0400

    add card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
+import Card from './Components/Card';
 import Hero from './Components/Hero';
 import Nav from './Components/Nav';
+import data from './data';
+console.log(data)
+
 function App() {
+  const items=data.map((item)=> {
+    return(
+      <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+      />
+    )
+  })
+ 
+
   return (
     <div className="App">
      <Nav/>
      <Hero/>
+     <section className='cards__list'>
+     {items}
+     </section>
     </div>
   );
 }
